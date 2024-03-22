d779fca966be7ddbb53a68f359a17d816ac47a14
Hammad Sheikh <hammadsheikh5237@gmail.com>
2024-03-23 02:16:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        message: "Enter First Number!",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter Second Number!",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "Select one of the Operators to perform operations!",
+        type: "list",
+        name: "operators",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+
+switch (answer.operators) {
+    case "+":
+        console.log("The Value of Addition is: " + (answer.firstNumber + answer.secondNumber));
+        break;
+    case "-":
+        console.log("The Value of Subtraction is: " +
+            (answer.firstNumber - answer.secondNumber));
+        break;
+    case "*":
+        console.log("The value of Multiplication is: " +
+            answer.firstNumber * answer.secondNumber);
+        break;
+    case "/":
+        console.log("The Value of Division is: " + answer.firstNumber / answer.secondNumber);
+        break;
+    default:
+        break;
+}
